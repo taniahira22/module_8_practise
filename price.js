b6fa5bff3dd01905ae9cce7dde382dbe3f16ccf3
taniahira22 <tania.hira2020@gmail.com>
2026-08-01 23:34:55 +0600

    first commit

---
+++ price.js
@@ -0,0 +1,24 @@
+let price1 = 20;
+let price2 = 20;
+console.log(price1 == price2); //comparison ==
+let price1 = 140;
+let price2= 100;
+console.log(price1 === price2);  //comparison ===
+let price1 = 100;
+let price2 = 140;
+console.log( price1 != price2 );//comparison !=
+let price1 = 20;
+let price2 = 10;
+console.log (price1 !== price2 );//comparison !==
+let price1 = 10;
+let price2 = 20;
+console.log(price1 > price2 );//comparison >
+let price1 = 60;
+let price2 = 40;
+console.log(price1 < price2 );// cpmparison <
+let price1 = 60;
+let price2 = 60;
+console.log(price1 >= price2 );//comparison >=
+let price1 = 60;
+let price2 = 80;
+console.log(price1 <= price2 );//comparison <=
